0c0822aadcc2635d4c290206cbb2ac9b0df187b2
thaint <truongthai960@gmail.com>
2024-10-21 20:48:24 +0700

    Update Bullet.cpp

--- AirPlane/Bullet.cpp
+++ AirPlane/Bullet.cpp
@@ -1,24 +1,19 @@
 #include "Bullet.h"
 
-Bullet::Bullet(float x ,float y)
-{
-	if (!texture.loadFromFile("../Data/laser.png")) {
-	}
-	sprite.setTexture(texture);
-	sprite.setPosition(x, y);
-	speed = 1.0f;
-
+Bullet::Bullet(float x, float y) {
+    if (!texture.loadFromFile("../Data/laser.png")) {}
+    sprite.setTexture(texture);
+    sprite.setPosition(x, y);
+    speed = 900.f;
 }
 
-
-void Bullet::update()
-{
-	sprite.move(speed, 0);
+void Bullet::update(float deltaTime) {
+    sprite.move(speed * deltaTime, 0);
 }
 
-void Bullet::render(RenderWindow& window)
-{
-	   window.draw(sprite);
+void Bullet::render(RenderWindow& window) {
+    window.draw(sprite);
 }
-
-
+FloatRect Bullet::getGlobalBounds() {
+    return sprite.getGlobalBounds(); // trả về vùng bao quanh đạn
+}
