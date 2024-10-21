b7fdbbf43d4c47d71797d3a6c7250bf87bdf925f
thaint <truongthai960@gmail.com>
2024-10-21 20:49:00 +0700

    Update Bullet.h

--- AirPlane/Bullet.h
+++ AirPlane/Bullet.h
@@ -1,15 +1,16 @@
-﻿#pragma once
-#include<SFML/Graphics.hpp>
+#pragma once
+#include <SFML/Graphics.hpp>
 using namespace sf;
+
 class Bullet {
 public:
-	Bullet (float x,float y);   // ham khoi tao
-	void update(); // ham cap nhat trang thai
-	void render(RenderWindow& window);// ham vẽ 
-
+    Bullet(float x, float y);
+    void update(float deltaTime);
+    void render(RenderWindow& window);
+    FloatRect getGlobalBounds(); // hàm lấy vùng bao quanh đạn
 
 private:
-	Texture texture;
-	Sprite sprite;
-	float speed;
+    Texture texture;
+    Sprite sprite;
+    float speed;
 };
