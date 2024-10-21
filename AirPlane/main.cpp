ae2503def632c6974e2f75d50cac4b8241aee198
thaint <truongthai960@gmail.com>
2024-10-21 20:49:22 +0700

    Update main.cpp

--- AirPlane/main.cpp
+++ AirPlane/main.cpp
@@ -1,26 +1,56 @@
-#include "player.h";
-const float Xmax=1500;
-const float Ymax=843;
-int main()
-{
-	RenderWindow window(VideoMode(1500,843), "AirPlane");
-	Texture background;
-	if (!background.loadFromFile("../Data/Clouds 1.png")) {
-		return -1;
-	}
-	Sprite bksprite;
-	bksprite.setTexture(background);
-	
-	Player player;
-	while (window.isOpen()) {
-		Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed)
-				window.close();
-		}
-		player.update();
-		window.draw(bksprite);
-		player.render(window);
-		window.display();
-	}
+#include "Player.h"
+#include "Enemy.h"
+#include <vector>
+#include "Score.h"
+
+int main() {
+    RenderWindow window(VideoMode(1500, 843), "AirPlane");
+
+    Texture background;
+    if (!background.loadFromFile("../Data/Clouds 1.png")) {
+        return -1;
+    }
+    Sprite bksprite;
+    bksprite.setTexture(background);
+
+    Player player;
+    Score score;
+    vector<Enemy> enemies(3);//tạo 3 kẻ thù
+
+
+    Clock clock;
+    while (window.isOpen()) {
+
+        float deltaTime = clock.restart().asSeconds();
+
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        player.update(deltaTime, enemies, score);
+
+        for (auto& enemy : enemies) {
+            enemy.update(deltaTime);
+        }
+        if (score.getScore() >= 200) {
+            score.reset(); // 200 đ thì reset lại
+            // thêm tính năng như quay lại menu chính hay sao thì thêm ở đây 
+        }
+
+
+        window.clear();
+
+        window.draw(bksprite);
+        player.render(window);
+        for (auto& enemy : enemies) {
+            enemy.render(window);
+        }
+        score.render(window);
+
+        window.display();
+    }
+
+
 }
