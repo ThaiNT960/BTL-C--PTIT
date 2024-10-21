67e063668126027450647f0dfa75e3968aa771c7
thaint <truongthai960@gmail.com>
2024-10-21 20:41:37 +0700

    Add files via upload

---
+++ AirPlane/Debug/Enemy.cpp
@@ -0,0 +1,35 @@
+﻿#include "Enemy.h"
+
+
+Texture Enemy::texture;
+
+Enemy::Enemy() {
+    if (!texture.loadFromFile("../Data/enemy2.png")) {}
+    sprite.setTexture(texture);
+
+    // random y từ 0 đến 750 
+    float randomY = rand() % 750;
+    sprite.setPosition(1500, randomY);
+    speed = 300.0f;
+}
+void Enemy::reset() {
+    // Đặt lại vị trí kẻ thù khi bị tiêu diệt or ra khỏi màn hình
+    float randomY = rand() % 750;
+    sprite.setPosition(1500, randomY); // xuất hiện lại bên phải màn hình
+}
+void Enemy::update(float deltaTime) {
+
+    sprite.move(-speed * deltaTime, 0);
+
+    // nếu kẻ thù ra khỏi màn hình, đặt lại vị trí
+    if (sprite.getPosition().x + sprite.getGlobalBounds().width < 0) {
+        reset();
+    }
+}
+
+void Enemy::render(RenderWindow& window) {
+    window.draw(sprite);
+}
+FloatRect Enemy::getGlobalBounds() {
+    return sprite.getGlobalBounds(); // trả về vùng bao quanh của kẻ thù
+}
---
+++ AirPlane/Debug/Score.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+using namespace std;
+using namespace sf;
+class Score {
+public:
+    Score();
+    void tăng(int tmp); // hàm tăng
+    void reset(); // reset điểm
+    void render(RenderWindow& window); 
+    int getScore() ; // lấy điểm hiện tại
+
+private:
+    int score; 
+    Font font; 
+    Text scoreText; 
+};
